c871e6c2bde1c7e822f4c266a6b0a605d62b65f8
qma <zndwlrnjr26@daum.net>
2022-04-02 19:37:38 +0900

    userProvider, userService - boilertemplate 추가 (초기설정)

---
+++ src/app/User/userProvider.js
@@ -0,0 +1,4 @@
+const { pool } = require('../../../config/database');
+
+const userDao = require('./userDao');
+
---
+++ src/app/User/userService.js
@@ -0,0 +1,5 @@
+const { pool } = require('../../../config/database');
+
+const userProvider = require('./userProvider');
+const userDao = require('./userDao');
+

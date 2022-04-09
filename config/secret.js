dafc600b9168b8ae254ff9c5c5ccb67a509f472d
qma <zndwlrnjr26@daum.net>
2022-04-09 17:29:20 +0900

    Remove all files that are in the .gitignore

--- config/secret.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    'jwtsecret' :  'swit_secret_key_2034012830',
-    'REST_API_KEY' : 'ac7e9b44dce14e93f3f3a0fe64e2dcec',
-    'REDIRECT_URI' : 'http://localhost:3000/kakao-login',
-};
